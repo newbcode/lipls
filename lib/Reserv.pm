1044714bd14570fb32ffc397ef95e1c323cffdd4
Yun Chang Kang <codenewb@gmail.com>
2016-07-20 00:44:06 +0900

    Youtube fucn add

---
+++ lib/Reserv.pm
@@ -0,0 +1,123 @@
+#===============================================================================
+#
+#         FILE: ReservDB.pm
+#
+#  DESCRIPTION: 예약페이지 DB API
+#
+#        FILES:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  newbcode@lilpls.com
+#      COMPANY:  Lipls
+#      VERSION:  0.001
+#      CREATED:  2016년 07월 10일
+#     REVISION:  ---
+#===============================================================================
+
+package Reserv;
+
+use Moose;
+use DBIx::Simple;
+use Data::Printer;
+use Date::Parse;
+
+has 'table_name' => (
+	is  => 'rw',
+	isa => 'Str',
+);
+
+has 'list_id' => (
+	is  => 'ro',
+	isa => 'Str',
+);
+
+# Customize log file location and minimum log level
+my $log = Mojo::Log->new(path => './log/mojo.log', level => 'info');
+
+#Mysql 접속
+my $dbh = DBIx::Simple->connect(
+        'DBI:mysql:database=lipls',
+	'lipls', 'lipls',
+        {   
+		#RaiseError => 1,
+		#mysql_enable_utf8 => 1,
+                mysql_auto_reconnect => 1,
+        }
+);
+
+sub t_write {
+	my ($self, $info, $loginname ) = @_;
+
+	my $table = $self->table_name();
+
+	if ( $table eq 'reserv_board' ) {
+		$dbh->query(
+			"INSERT INTO $table (`name`, `subject`, `phone`, `pay_name`, `use_cnt`, `shot_desc`, `pw`, `start_date`, `end_date`) VALUES (??)",
+			$info->{name}, $info->{subj}, $info->{phone}, $info->{pay_name}, $info->{use_cnt}, 
+			$info->{shot_desc}, $info->{pw}, $info->{start_date}, $info->{end_date}
+		);
+	}
+	else {
+		$dbh->query(
+			"INSERT INTO $table (`reserv_id`, `name`, `reply`) VALUES (??)",
+			$info->{notice_id}, $loginname, $info->{content}             
+		) or die $dbh->error;
+	}
+
+	unless ( $dbh->error ) {
+		print "ERROR\n";
+		return $dbh->error;
+	}
+
+	return 1;
+}
+
+sub t_read {
+	my $self = shift;
+
+	my $table = $self->table_name;
+	my $id = $self->list_id;
+
+	if ( $table eq 'reserv_board' ) {
+		my $h = $dbh->query( "SELECT * FROM $table WHERE id=$id") or die $dbh->error;
+		my @result = $h->hashes;
+
+		return @result;
+	}
+	else {
+		my $reply_rows = $dbh->query("SELECT * FROM $table WHERE reserv_id=$id") or die $dbh->error;
+		my $replys;
+		while( my $row = $reply_rows->hash) {
+			my $datetime = str2time("$row->{wdate} -0900");
+			#$log->info("date: $datetime");
+			$replys->{$datetime} = $row;
+			#$log->info("RE: $replys->{$datetime}")
+		}
+		
+		return $replys;
+	}
+}
+
+sub t_del {
+	my $self = shift;
+
+	my $table = $self->table_name();
+	my $id = $self->list_id();
+
+	$dbh->query( "DELETE FROM $table WHERE id=$id") or die $dbh->error;
+
+	return 1;
+}
+
+sub t_list {
+	my $self = shift;
+
+	my $table = $self->table_name();
+
+	my $h = $dbh->query( "SELECT `id`, `name`, `subject`, `wdate` FROM $table") or die $dbh->error;
+	my @result = $h->hashes;
+
+	return @result;
+}
+
+1;
--- lipls.pl
+++ lipls.pl
@@ -1,122 +1,174 @@
 #!/usr/bin/env perl
 
 use Mojolicious::Lite;
-use Data::Printer;
 use JSON;
 use Moose;
+use Mojo::Log;
 use Encode;
+use Data::Printer;
 use lib "/home/newbcode/server/lipls/lib";
 
-use ReservDB;
+use Reserv;
 
 # Documentation browser under "/perldoc"
 plugin 'PODRenderer';
+my $selfonfig = plugin 'Config';
 
-my $config = plugin 'Config';
+# Customize log file location and minimum log level
+my $log = Mojo::Log->new(path => './log/mojo.log', level => 'info');
 
+# Main
 get '/' => sub {
-	my $c = shift;
+	my $self = shift;
 
 #} => 'coming_soon';
 } => 'index';
 
 get '/about_us' => sub {
-	my $c = shift;
+	my $self = shift;
 
 };
 
 get '/services' => sub {
-	my $c = shift;
+	my $self = shift;
 
 };
 
 get '/portfolio' => sub {
-	my $c = shift;
+	my $self = shift;
 
 } => 'portfol';
 
-get '/reserv' => sub {
-	my $c = shift;
+get '/studio' => sub {
+	my $self = shift;
 
-	my @r = ReservDB->new( table_name => 'reserv_board' )->t_list();
-	$c->stash( list => \@r );
+};
+
+get '/reserv' => sub {
+	my $self = shift;
+
+	my @r = Reserv->new( table_name => 'reserv_board' )->t_list();
+	$self->stash( list => \@r );
 
 } => '/reserv/reserv';
 
 get '/contact' => sub {
-	my $c = shift;
+	my $self = shift;
 
 } => '/contact/contact';
 
 get '/reserv_info' => sub {
-	my $c = shift;
+	my $self = shift;
 
 } => '/reserv/reserv_info';
 
 get '/reserv_write' => sub {
-	my $c = shift;
+	my $self = shift;
 
 } => '/reserv/reserv_write';
 
 get '/reserv_read_check' => sub {
-	my $c = shift;
+	my $self = shift;
 
-	my $id = $c->param('id');
-	$c->stash( id => $id );
+	my $id = $self->param('id');
+	$self->stash( id => $id );
 
 } => '/reserv/reserv_read_check';
 
 post '/reserv_read_check' => sub {
-	my $c = shift;
+	my $self = shift;
 
-	my $info = $c->param('POSTDATA');
+	my $info = $self->param('POSTDATA');
 	my $r_info = from_json($info);
 
 	my $resp = { result  => 'N' };
-	my @r = ReservDB->new( list_id => $r_info->{id}, table_name => 'reserv_board' )->t_read();
+	my @r = Reserv->new( list_id => $r_info->{id}, table_name => 'reserv_board' )->t_read();
 
 	foreach my $k ( @r ) {
 		if ( $r_info->{pw} eq $k->{pw} ) {
-			#$c->redirect_to("/reserv_read_check?id=$r_info->{id}&pw=$r_info->{pw}");
+			#$self->redirect_to("/reserv_read_check?id=$r_info->{id}&pw=$r_info->{pw}");
+			my $n = decode('utf-8', $k->{name});
+			$self->session( name => $n );
+			$resp = { result  => 'Y' };
+		}
+		elsif ( $r_info->{pw} eq 'lipls0525') {
+			$self->session( name => 'LIPLS');
 			$resp = { result  => 'Y' };
 		}
 	}
-	$c->render(json => $resp);
+	$self->render(json => $resp);
 };
 
 get '/reserv_del' => sub {
-	my $c = shift;
+	my $self = shift;
 
-	my $id = $c->param('id');
-	my $r = ReservDB->new( list_id => $id, table_name => 'reserv_board' )->t_del();
-	$c->redirect_to("/reserv");
+	my $id = $self->param('id');
+	my $r = Reserv->new( list_id => $id, table_name => 'reserv_board' )->t_del();
+	$self->redirect_to("/reserv");
 };
 
 get '/reserv_read' => sub {
-	my $c = shift;
+	my $self = shift;
 
-	my $id  = $c->param('id');
-	my @r = ReservDB->new( list_id => $id, table_name => 'reserv_board' )->t_read();
+	my $id  = $self->param('id');
+	my @r = Reserv->new( list_id => $id, table_name => 'reserv_board' )->t_read();
 
-	$c->stash( read  => \@r );
+	$self->stash( read  => \@r );
 
 } => '/reserv/reserv_read_table';
 
 post '/reserv_write' => sub {
-	my $c = shift;
+	my $self = shift;
 
-	my $info = $c->param('POSTDATA');
+	my $info = $self->param('POSTDATA');
 	my $r_info = from_json($info);
 
 	my $resp = { result  => 'Y' };
 	foreach my $k ( keys %{$r_info} ) {
+		next if $k =~ /use_cnt|shot_desc/;
 		unless ( $r_info->{$k} ) {
 			$resp = { result => 'N' };
-			$c->render(json => $resp);
+			$self->render(json => $resp);
 		}
 	}
-	my $r = ReservDB->new( table_name => 'reserv_board' )->t_write( $r_info );
-	$c->render(json => $resp);
+	my $r = Reserv->new( table_name => 'reserv_board' )->t_write( $r_info );
+	$self->render(json => $resp);
+};
+
+# 예약 게시판 댓글 불러오기
+get '/reserv_reply' => sub {
+
+	my $self = shift;
+	my $id = $self->param('notice_id');
+
+	$log->info("NOTICE: $id");
+	my $replys = Reserv->new( list_id => $id, table_name => 'reserv_board_reply' )->t_read();
+
+	$self->stash( replys => $replys, notice_id => $id );
+} => 'reserv_reply';
+
+# 예약 게시판 댓글 저장
+post '/reserv_reply' => sub {                                                 
+	my $self = shift;
+
+	my $selfontent = $self->param('POSTDATA');                             
+	my $resp = {
+		result => 'N',
+	};
+
+	my $jdata = from_json($selfontent);
+
+	#$log->info("notice_id=$jdata->{notice_id}, content=$jdata->{content}");
+
+	if(exists $jdata->{notice_id} && exists $jdata->{content}) {
+		my $r = Reserv->new( table_name => 'reserv_board_reply' )->t_write( $jdata, $self->session('name') );
+		$resp->{result} = 'Y';
+	}
+	else {
+		$resp->{error} = 'argument error';
+	}       
+
+	$self->render(json => $resp);
 };
 
 app->start;
